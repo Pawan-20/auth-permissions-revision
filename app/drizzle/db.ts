98cb5325c70c1a9fe166c5da61b5f25201943d99
Pawan-20 <pawankumar85540@gmail.com>
2025-12-13 15:43:26 +0530

    [init]:: basic setup  for db, next app , drizzle

---
+++ app/drizzle/db.ts
@@ -0,0 +1,4 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/node-postgres";
+import * as schemas from "./schema";
+const db = drizzle(process.env.DATABASE_URL!, { schema: schemas });
